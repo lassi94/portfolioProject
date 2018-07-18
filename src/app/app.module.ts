39c5003a76c96f4debd4539b4cd7a3460de2117c
unknown <lasiko87@wippies.com>
2018-07-18 22:52:41 +0300

    made footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from './home/home.component';
 import { BlogComponent } from './blog/blog.component';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { SkillsComponent } from './skills/skills.component';
     HomeComponent,
     BlogComponent,
     AboutComponent,
-    SkillsComponent
+    SkillsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
